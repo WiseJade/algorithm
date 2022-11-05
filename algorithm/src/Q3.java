19366bcf486741d8fe485cc76eefe8df2fe17a5e
WiseJade <jeonhr861@gmail.com>
2022-11-05 21:58:52 +0900

    woowacourse-precourse problem3 풀이 추가

---
+++ algorithm/src/Q3.java
@@ -0,0 +1,11 @@
+public class Q3 {
+    int Solution (int number) {
+        int result = 0;
+        for (int i = 3; i <= number; i++) {
+            String snum = String.valueOf(i);
+            if (snum.contains("3") || snum.contains("6") || snum.contains("9"))
+                result++;
+        }
+        return result;
+    }
+}
---
+++ algorithm/src/Q3Test.java
@@ -0,0 +1,7 @@
+public class Q3Test {
+    public static void main(String[] args) {
+        Q3 q3 = new Q3();
+        System.out.println(q3.Solution(13));
+        System.out.println(q3.Solution(33));
+    }
+}
