9c025c5cc368d64cc15a66e166bb325e1bd88b97
WiseJade <jeonhr861@gmail.com>
2022-11-06 00:07:03 +0900

    woowacourse-precourse Problem6 풀이 -> 테스트 통과

---
+++ algorithm/src/woowacourse_precourse/Q6.java
@@ -0,0 +1,28 @@
+package woowacourse_precourse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Q6 {
+    public List<String> Solution(String[][] forms) {
+        // 순서대로 이름 조합, 이메일
+        Map<String, String> map = new HashMap();
+        List<String> result = new ArrayList<>(forms.length);
+        for (String[] form : forms) {
+            for (int i = 0; i < form[1].length() - 1; i++) {
+                String nameSeq = form[1].substring(i, i + 2);
+                if (map.containsKey(nameSeq)) {
+                    if (!result.contains(map.get(nameSeq))) {
+                        result.add(map.get(nameSeq));
+                    }
+                    result.add(form[0]);
+                } else {
+                    map.put(nameSeq, form[0]);
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ algorithm/src/woowacourse_precourse/Q6Test.java
@@ -0,0 +1,18 @@
+package woowacourse_precourse;
+
+import java.util.Arrays;
+
+public class Q6Test {
+    public static void main(String[] args) {
+        Q6 q6 = new Q6();
+        String user = "mrko";
+        String[][] friends = {
+                new String[]{"jm@email.com", "제이엠"},
+                new String[]{"jason@email.com", "제이슨"},
+                new String[]{"woniee@email.com", "워니"},
+                new String[]{"mj@email.com", "엠제이"},
+                new String[]{"nowm@email.com", "이제엠"},
+        };
+        System.out.println(q6.Solution(friends).toString());
+    }
+}
