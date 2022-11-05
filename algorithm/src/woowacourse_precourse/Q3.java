28b4a7cf58af2da92735ac4d9bf08d8c53cb84b0
WiseJade <jeonhr861@gmail.com>
2022-11-05 22:59:23 +0900

    fix: Problem3 수정

--- algorithm/src/woowacourse_precourse/Q3.java
+++ algorithm/src/woowacourse_precourse/Q3.java
@@ -5,8 +5,10 @@ public class Q3 {
         int result = 0;
         for (int i = 3; i <= number; i++) {
             String snum = String.valueOf(i);
-            if (snum.contains("3") || snum.contains("6") || snum.contains("9"))
-                result++;
+            for (int j = 0; j < snum.length(); j++) {
+                if (snum.charAt(j) == '3' || snum.charAt(j) == '6' || snum.charAt(j) == '9')
+                    result++;
+            }
         }
         return result;
     }
