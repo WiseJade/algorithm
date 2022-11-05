690d55f559a9ffa2354fa1559dd96f17d60b8c95
WiseJade <jeonhr861@gmail.com>
2022-11-05 09:04:39 +0900

    woowacourse-precource PROBLEM1 풀이

---
+++ algorithm/src/Q1.java
@@ -0,0 +1,28 @@
+public class Q1 {
+    int solution (int[] pobi, int[] crong) {
+        if (!(pobi.length == 2) || !(crong.length == 2)) return -1;
+        if (pobi[0] % 2 == 0 || !(pobi[1] == pobi[0] + 1) || crong[0] % 2 == 0 || !(crong[1] == crong[0] + 1)) return -1;
+        int pobiMax = 0;
+        int crongMax = 0;
+        pobiMax = findMax(pobi[0]);
+        crongMax = findMax(crong[0]);
+        pobiMax = Math.max(pobiMax, findMax(pobi[1]));
+        crongMax = Math.max(crongMax, findMax(crong[1]));
+        if (pobiMax > crongMax) return 1;
+        else if (pobiMax < crongMax) return 2;
+        else if (pobiMax == crongMax) return 0;
+        else return -1;
+    }
+
+    // 숫자를 입력받아 각자리 숫자의 합과 곱 중 더 큰 수를 반환하는 메소드
+    int findMax (int num) {
+        int add = 0;
+        int multiple = 1;
+        char[] numArr = String.valueOf(num).toCharArray();
+        for (char c : numArr) {
+            add += c - '0';
+            multiple *= c - '0';
+        }
+        return Math.max(add, multiple);
+    }
+}
---
+++ algorithm/src/Q1Test.java
@@ -0,0 +1,8 @@
+public class Q1Test {
+    public static void main(String[] args) {
+        Q1 q1 = new Q1();
+        System.out.println(q1.solution(new int[]{97, 98}, new int[]{197, 198}));
+        System.out.println(q1.solution(new int[]{131, 132}, new int[]{211, 212}));
+        System.out.println(q1.solution(new int[]{99, 102}, new int[]{211, 212}));
+    }
+}
